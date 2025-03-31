c4f569f115efabfb86486e133422fd35c51eaba3
octaviopereira10 <octaviopereira10@gmail.com>
2025-03-31 12:30:56 +0000

    primeiro commit do projeto, base do html e inicio do css

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+
+export function App() {
+  return(
+    <div className="h-svh bg-very-dark-blue text-white flex justify-center items-center px-6">
+      <div className="bg-blue max-w-103">
+        
+
+        <h1>How did we do?</h1>
+
+        <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+      
+      <div>
+        <input type="button" value={1} />
+        <input type="button" value={2} />
+        <input type="button" value={3} />
+        <input type="button" value={4} />
+        <input type="button" value={5} />
+      </div>
+
+      <button>Submit</button>
+      </div>
+    </div>
+  )
+}
