97d9421a577c5429d1b42fb5a04fa4f6df945d47
octaviopereira10 <octaviopereira10@gmail.com>
2025-04-14 18:29:54 +0000

    separaçao dos componenetes Cardincial,Cardfinal e InputButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
 
+import { useState } from "react"
+import { CardFinal } from "./components/CardFinal"
+import { CardInicial } from "./components/CardInicial"
 
-export function App() {
+
+
+
+export function App(){
+const [submited, setSubmited] = useState(false)
+const [rateNote,setRateNote] = useState(0)
+
+
+function handleSubmit(){
+ if(rateNote !== 0){
+  setSubmited(true)
+  return
+ }
+ alert("please,choose a rate button! ")
+}
+
+function handleChangeRateNote(value){
+  
+  setRateNote(value)
+}
+
   return(
-    <div className="h-svh bg-very-dark-blue text-white flex justify-center items-center px-6">
-      <div className="bg-blue max-w-103">
-        
+ 
+     
+     submited === false ? (
 
-        <h1>How did we do?</h1>
+      <CardInicial handleChangeRateNote={handleChangeRateNote} handleSubmit={handleSubmit} />
+     
+     ) : (
+ 
+      <CardFinal rateNote={rateNote} />
 
-        <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-      
-      <div>
-        <input type="button" value={1} />
-        <input type="button" value={2} />
-        <input type="button" value={3} />
-        <input type="button" value={4} />
-        <input type="button" value={5} />
-      </div>
-
-      <button>Submit</button>
-      </div>
-    </div>
+     )
+ 
   )
 }
